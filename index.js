d21558809fa1f5e19b2deefed5b0612067dfc36c
eric <contreras.eric18@gmail.com>
2021-08-10 14:20:29 -0400

    commit tree 8d0d2ac9a0609a20b57fc2dff834908c2e85679b

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '');
+  }
+
+  static titleize(string){
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let finalTitle = [];
+
+    let array = string.split(" ");
+    for(let i = 0; i < array.length; i++){
+      if(i === 0){
+        finalTitle.push( this.capitalize(array[i]));
+      }
+      else{
+        if( exceptions.includes(array[i])){
+          finalTitle.push( array[i]);
+        }
+        else {
+          finalTitle.push( this.capitalize(array[i]));
+        }
+      }
+    }
+    return finalTitle.join(" ");
+  }
 }
